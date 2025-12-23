71ec4473eb3d24fc5f625a7c7df81ebafeb9e059
itsyeren <itsyeren@gmail.com>
2025-12-23 19:26:11 +0300

    Remove solution folder

--- solution/solution_02-Data-Toolkit_04-Statistics-Probabilities_01-Law-of-Large-Numbers/README.md
+++
@@ -1,5 +0,0 @@
- `law_of_large_numbers.ipynb` adlı jupyter notebook dosyasını aç ve talimatları takip et.
- 
-```python
-jupyter notebook
-```
--- solution/solution_02-Data-Toolkit_04-Statistics-Probabilities_01-Law-of-Large-Numbers/tests/test_expected_value_coins.py
+++
@@ -1,9 +0,0 @@
-from nbresult import ChallengeResultTestCase
-import random
-
-
-class TestExpectedValueCoins(ChallengeResultTestCase):
-
-    def test_expected_value_coins(self):
-        expected_value_coins = 7
-        self.assertEqual(self.result.expected_value_coins, expected_value_coins)
