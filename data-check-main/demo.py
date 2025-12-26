353c91a3814664da1d67a087c2a30d5a1f42db6d
itsyeren <itsyeren@gmail.com>
2025-12-27 00:44:40 +0300

    circle_area_calculation_exp

---
+++ data-check-main/demo.py
@@ -0,0 +1,8 @@
+import math
+
+def circle_area(radius):
+    """Returns the area of the circle of given radius"""
+    if radius > 0:
+        return math.pi * radius**2
+    else:
+        return 0
